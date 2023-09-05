521727ef2ae2f6452b9142050b0106420848d075
Arthur-Legal <135077133+Arthur-Legal@users.noreply.github.com>
2023-09-05 18:33:10 -0300

    Update index.php

--- ppa/back/ppa_api_calendario/index.php
+++ ppa/back/ppa_api_calendario/index.php
@@ -1,1 +1,31 @@
+<?php
+
+use Api\Http\Router;
+use Api\Utils\View;
+use Api\Controller\pages\Home;
+
+require __DIR__ . '/config/app.php';
+
+#define('URL','http://localhost/ppa_api_calendario');
+
+//INICIA O ROUTER
+$obRouter = new Router(URL);
+
+/**
+ * exit;
+ * echo Home::getHome();
+ */
+
+View::init([
+    'URL' => URL
+]);
+
+
+
+//INCLUI AS ROTAS DA API
+#include __DIR__ . '/Api/Routes/pages.php';
+
+// IMPRIME O RESPONSE DA ROTA
+$obRouter->run()
+  ->sendResponse();
 
